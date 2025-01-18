21303daf28d382a61d780f8996c5639699b076e4
Muhammad Akif Hussain <makifhussainmn@gmail.com>
2025-01-18 18:42:26 +0500

    3  commit

--- app.js
+++ app.js
@@ -1,71 +1,61 @@
- let userscore = document.querySelector("#user-score");
- let Computerscore = document.querySelector("#Computer-score");
- let choices = document.querySelectorAll(".choice");
- let msg = document.querySelector("#msg");
+let userscore = document.querySelector("#user-score");
+let Computerscore = document.querySelector("#Computer-score");
+let choices = document.querySelectorAll(".choice");
+let msg = document.querySelector("#msg");
 
+let userscorepar = 0;
+let Computersscorepar = 0;
+const Computerchoice = () => {
+  const options = ["rock", " paper", "scissors"];
+  const random1 = Math.floor(Math.random() * 3);
+  return options[random1];
+};
 
- let userscorepar = 0;
- let Computersscorepar = 0;
- const  Computerchoice = () =>{
-    const options= ["rock" ," paper" ,"scissors"];
-    const random1 = Math.floor(Math.random()* 3);
-      return options[random1]
+const gamedrw = () => {
+  console.log("game are draw.");
+  msg.innerText = "Game are draw , Play Again";
+  msg.style.backgroundColor = "blue";
+};
 
-} 
+showwiner = (userwin21, userchoice, Computerchoice1) => {
+  if (userwin21) {
+    userscorepar++;
+    userscore.innerText = userscorepar;
+    msg.innerHTML = `You Win, Your ${userchoice}, Beat ${Computerchoice1}`;
+    msg.style.backgroundColor = "green";
+  } else {
+    Computersscorepar++;
+    Computerscore.innerText = Computersscorepar;
+    console.log("you lose ");
+    msg.innerText = `You lose  ${Computerchoice1}  Beats ${userchoice} `;
+    msg.style.backgroundColor = "red";
+  }
+};
 
-const gamedrw = ()=>{
-    console.log("game are draw.")
-     msg.innerText = "Game are draw , Play Again"
-     msg.style.backgroundColor = "blue"
-}
-
-showwiner = (userwin21 , userchoice , Computerchoice1)=>{
-    if(userwin21){
-        userscorepar++
-        userscore.innerText = userscorepar; 
-        msg.innerHTML = `You Win, Your ${userchoice}, Beat ${Computerchoice1}`;
-        msg.style.backgroundColor = "green"
+const playGame = (userchoice) => {
+  console.log(userchoice);
+  const Computerchoice1 = Computerchoice();
+  console.log(" computer", Computerchoice1);
+  if (userchoice === Computerchoice1) {
+    gamedrw();
+  } else {
+    let userwin21 = true;
+    if (userchoice === "rock") {
+      userwin21 = Computerchoice1 === "paper" ? false : true;
+    } else if (userchoice === "paper") {
+      userwin21 = Computerchoice1 === "scissors" ? false : true;
     } else {
-        Computersscorepar++
-        Computerscore.innerText = Computersscorepar; 
-    console.log("you lose ")
-    msg.innerText =`You lose  ${Computerchoice1}  Beats ${userchoice} ` 
-    msg.style.backgroundColor = "red"
+      userwin21 = Computerchoice1 === "rock" ? false : true;
     }
-}
+    showwiner(userwin21, userchoice, Computerchoice1);
+  }
+};
 
-const playGame = (userchoice) =>{
-    console.log(userchoice);
-    const Computerchoice1 = Computerchoice() ; 
-    console.log( " computer",  Computerchoice1 )
-if( userchoice === Computerchoice1)
-{
-    gamedrw()
-}
-else {
-    let userwin21 = true ;
-    if(userchoice === "rock"){
-        userwin21 = Computerchoice1 === "paper" ? false :  true
-    }
-    else if(userchoice === "paper"){
-        
-        userwin21 = Computerchoice1 === "scissors" ? false :  true
-    }
-    else {
-        
-        userwin21 = Computerchoice1 === "rock" ? false :  true
-    }
-    showwiner(userwin21 , userchoice , Computerchoice1)
-
-}
-}
-
- choices.forEach( (choice) =>{
-    console.log(choice)
-    choice.addEventListener('click' , ()=>{
-       
-       const userchoice = choice.getAttribute("ID")
-       playGame(userchoice)
+choices.forEach((choice) => {
+  console.log(choice);
+  choice.addEventListener("click", () => {
+    const userchoice = choice.getAttribute("ID");
+    playGame(userchoice);
     //    playGame()
-    })
- })
+  });
+});
